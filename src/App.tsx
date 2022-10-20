6634dbec144ac92b52a1e37d83b762ffd7bbfd83
leonardo dias <leonardodias@ucl.br>
2022-10-20 19:38:58 -0300

    create login page with components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,67 @@
+import { Envelope, Lock } from 'phosphor-react';
+import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import { Logo } from './Logo';
 import './styles/global.css';
+
 
 
 export function App() {
   return (
-    <div>
-      <h1 className=''>Hello World</h1>
-      <button className='bg-violet-500 px-4 py-3 rounded text-white'>
-       enviar 
-      </button>
+    <div className='w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100'>
+      <header className='flex flex-col items-center'>
+        <Logo />
+
+        <Heading size='lg' className='mt-4'>
+          Ignite Lab
+        </Heading>
+
+        <Text size='lg' className='text-gray-400 mt-1'>
+          Lorem ipsum dolor sit amet consectetur.
+        </Text>
+      </header>
+
+      <form className="flex flex-col items-stretch w-full max-w-[400px] mt-10 gap-4">
+        <label htmlFor='email' className='flex flex-col gap-3'>
+          <Text className='font-semibold'>Email:</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+
+            <TextInput.Input id='email' placeholder='example@email.com' />
+          </TextInput.Root>
+        </label>
+        <label htmlFor='password' className='flex flex-col gap-3'>
+          <Text className='font-semibold'>Password:</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+
+            <TextInput.Input id='password' placeholder='************' />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor='remember' className='flex items-center gap-2'>
+          <Checkbox id='remember' />
+          <Text size='sm' className='text-gray-200'>Remember me for 30 days</Text>
+        </label>
+
+        <Button type="submit" className='mt-4'>Login</Button>
+      </form>
+
+      <footer className='flex flex-col items-center gap-4 mt-8'>
+        <Text size='sm' asChild>
+          <a href='' className='text-gray-400 underline hover:text-gray-200'>Forgot your password?</a>
+        </Text>
+        <Text size='sm' asChild>
+          <a href='' className='text-gray-400 underline hover:text-gray-200'>Don't have an account? Sign up for free!</a>
+        </Text>
+      </footer>
     </div>
   )
 }
